90e3aeea2c46980fdba6e085d9a5fc6e6cfebef2
artemleschenko <artem.leschenko@innowise.com>
2025-12-18 12:16:54 +0300

    feat: add uploaders and change gitignore

--- lib/src/core_ui/widgets/widgets.dart
+++ lib/src/core_ui/widgets/widgets.dart
@@ -2,3 +2,5 @@ export 'app_text_field.dart';
 export 'custom_checkbox.dart';
 export 'submit_button.dart';
 export 'app_date_time_field.dart';
+export 'upload_image.dart';
+export 'file_upload.dart';
